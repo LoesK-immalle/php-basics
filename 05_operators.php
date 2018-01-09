e815236b4454380a4aab50f3f079ed9c759fef72
LoesK-immalle <loes.kenis@immalle.be>
2018-01-09 15:01:54 +0100

    Finished assignment 05

--- 05_operators.php
+++ 05_operators.php
@@ -5,18 +5,18 @@
 
 // Bereken de 5e macht van 2.
 
-
+echo pow(2,5). "<br>";
 
 $var_a = "123";
 $var_b = 123;
 
 // Vervang de true in de if-voorwaarden zodat het klopt wat er staat:
 
-if(true) {
+if($var_a == $var_b) {
     echo '$var_a en $var_b zijn aan elkaar gelijk maar zijn niet van hetzelfde type.';
 }
 
-if(true) {
+if($var_a === $var_b) {
     echo '$var_a en $var_b zijn aan elkaar gelijk en van hetzelfde type.';
 }
 
